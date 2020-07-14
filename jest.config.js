162a6cf892a544681797f12ac9492f5da80e52c2
Paulo Henrique <paulohenriq.dev@gmail.com>
2020-07-13 22:53:32 -0300

    refactor: adjusting the jest config

--- jest.config.js
+++ jest.config.js
@@ -8,15 +8,17 @@ module.exports = {
   collectCoverage: true,
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    '<rootDir>/versions/v1/**/**.js',
+    '<rootDir>/versions/v1/src/controllers/**.js',
+    '<rootDir>/versions/v1/src/services/**.js',
+    '!<rootDir>/versions/v1/config.js',
+    '!<rootDir>/versions/v1/routes.js',
+    '!<rootDir>/versions/v1/swagger/**',
     '!<rootDir>/versions/v1/test/**',
   ],
   // The directory where Jest should output its coverage files
   coverageDirectory: '<rootDir>/versions/v1/test/coverage',
   // The test environment that will be used for testing
   testEnvironment: 'node',
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['<rootDir>/versions/v1/**/*.spec.js'],
 
   testTimeout: 60000, // Increased minimum timeout
 };
